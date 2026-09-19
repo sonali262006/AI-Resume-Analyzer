48bc24c086a4fb824952a1bbb06fad950073c048
Sonu <sonalidilipsing2006@gmail.com>
2026-09-19 15:47:33 +0530

    Connect frontend to live backend

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://ai-resume-analyzer-rtap.onrender.com";;
 
 export async function analyzeResume(file) {
   const formData = new FormData();
